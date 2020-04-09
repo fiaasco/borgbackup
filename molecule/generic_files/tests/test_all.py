f48a36ecf1a0240c31961d518696dcd9198711d5
Dieter Verhelst <info@werus.be>
2020-04-09 21:29:45 +0200

    Test packaged installations

--- molecule/generic_files/tests/test_all.py
+++ molecule/generic_files/tests/test_all.py
@@ -1,6 +1,18 @@
-def test_borg_binary(host):
-    borg = host.file("/usr/local/bin/borg")
-    assert borg.exists
-    assert borg.user == "root"
-    assert borg.group == "root"
-    assert borg.mode == 0o755
+import pytest
+from testinfra.utils.ansible_runner import AnsibleRunner
+
+testinfra_hosts = ["ansible://all"]
+
+
+def test_borg_package(host):
+    ans_dict = host.ansible.get_variables()
+    package = ans_dict['borgbackup_install_from_pkg']
+    if package is True:
+      borg = host.package("borgbackup")
+      assert borg.is_installed
+    else:
+      borg = host.file("/usr/local/bin/borg")
+      assert borg.exists
+      assert borg.user == "root"
+      assert borg.group == "root"
+      assert borg.mode == 0o755
