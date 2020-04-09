19c396bc16c9b2ebbe26c7f2a51deec29b40d6a5
Dieter Verhelst <info@werus.be>
2020-04-09 21:31:37 +0200

    Linting fixes on default test

--- molecule/generic_files/tests/test_all.py
+++ molecule/generic_files/tests/test_all.py
@@ -1,6 +1,3 @@
-import pytest
-from testinfra.utils.ansible_runner import AnsibleRunner
-
 testinfra_hosts = ["ansible://all"]
 
 
@@ -8,11 +5,11 @@ def test_borg_package(host):
     ans_dict = host.ansible.get_variables()
     package = ans_dict['borgbackup_install_from_pkg']
     if package is True:
-      borg = host.package("borgbackup")
-      assert borg.is_installed
+        borg = host.package("borgbackup")
+        assert borg.is_installed
     else:
-      borg = host.file("/usr/local/bin/borg")
-      assert borg.exists
-      assert borg.user == "root"
-      assert borg.group == "root"
-      assert borg.mode == 0o755
+        borg = host.file("/usr/local/bin/borg")
+        assert borg.exists
+        assert borg.user == "root"
+        assert borg.group == "root"
+        assert borg.mode == 0o755
